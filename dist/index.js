22e6ee4e5b71e461b0f78dde89e21d55e3fff263
Joel Gimbel <jgimbel@live.com>
2016-06-27 00:42:42 +0000

    no, but really, fix the argumetns

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var DEFAULT_ADDRESS = 0x48,
@@ -60,7 +62,7 @@ var ADS1x15 = function () {
   function ADS1x15() {
     _classCallCheck(this, ADS1x15);
 
-    this._device = new (Function.prototype.bind.apply(i2c, [null].concat(Array.prototype.slice.call(arguments))))();
+    this._device = new (Function.prototype.bind.apply(i2c, [null].concat(_toConsumableArray(arguments[0]))))();
   }
 
   _createClass(ADS1x15, [{
